7e4a5d0da0f973756b17061ec6322354051aa072
Dace Brakandaran <jayler.school@gmail.com>
2019-11-11 10:55:20 -0800

    addPart function

---
+++ addPart.m
@@ -0,0 +1,17 @@
+function [] = addPart()
+% ADDPART a function of Parts_Tracker_Editor that adds new parts to an
+% existing parts tracker sheet
+
+%   Takes no input initially, user will specify needs while function is
+%   running
+
+%% What we need to know.
+% sheet name
+fprintf('Check that tracker sheet is in the current folder.\n');
+sheet_name = input('What is the name of the file? (NAME.xlsx): ', 's');
+
+% part type
+
+
+end
+
